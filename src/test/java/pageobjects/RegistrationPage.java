a1de4646060fb81b7b715014a6835565d97521ea
Yasmany <yasmanys88@gmail.com>
2022-02-11 00:00:01 -0300

    Applying Page Factory in RegistrationPage

--- src/test/java/pageobjects/RegistrationPage.java
+++ src/test/java/pageobjects/RegistrationPage.java
@@ -1,40 +1,109 @@
 package pageobjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
 public class RegistrationPage {
-    //div/div[3]/div/ul/li[1] email
-    /*
-    * //*[@id="core__protected_modules_user_yiiForm_RegistrationForm_email"]
-    * */
+    @FindBy(how= How.XPATH,using = "//div/div[3]/div/ul/li[1]")
+    private WebElement optionEmail;
 
-    //div/div[3]/div/ul/li[1] Phonenumber
-    /*
-     * //*[@id="core__protected_modules_user_yiiForm_RegistrationForm_phone"]
-     * */
+    @FindBy(how= How.XPATH,using = "//div/div[3]/div/ul/li[2]")
+    private WebElement optionPhoneNumber;
 
-    /*
-    USD EUR mBTC mETH
-    * //form/fieldset[2]/div[1]/div[1]/div[1]/label
-    * //form/fieldset[2]/div[1]/div[1]/div[2]/label
-    * //form/fieldset[2]/div[1]/div[1]/div[3]/label
-    * //form/fieldset[2]/div[1]/div[1]/div[4]/label
-    *
-    * */
+    @FindBy(how= How.ID,using = "core__protected_modules_user_yiiForm_RegistrationForm_email")
+    private WebElement inputEmail;
 
-    //password
-    //*[@id="core__protected_modules_user_yiiForm_RegistrationForm_password"]
+    @FindBy(how= How.ID,using = "core__protected_modules_user_yiiForm_RegistrationForm_phone")
+    private WebElement inputPhoneNumber;
 
-    //password_confirmation
-    ////*[@id="core__protected_modules_user_yiiForm_RegistrationForm_password_confirmation"]
+    @FindBy(how= How.XPATH,using = "//form/fieldset[2]/div[1]/div[1]/div[1]/label")
+    private WebElement optionUSD;
 
-    //label agree
-    //div/div[2]/form/fieldset[2]/div[4]/label
+    @FindBy(how= How.XPATH,using = "//form/fieldset[2]/div[1]/div[1]/div[2]/label")
+    private WebElement optionEUR;
 
-    //Form_RegistrationForm_captcha
-    //*[@id="core__protected_modules_user_yiiForm_RegistrationForm_captcha"]
+    @FindBy(how= How.XPATH,using = "//form/fieldset[2]/div[1]/div[1]/div[3]/label")
+    private WebElement optionmBTC;
 
-    //button Register
-    //form/fieldset[3]/button
+    @FindBy(how= How.XPATH,using = "//form/fieldset[2]/div[1]/div[1]/div[4]/label")
+    private WebElement optionmETH;
 
-    //sign in link
-    //form/div[2]/a
+    @FindBy(how= How.ID,using = "core__protected_modules_user_yiiForm_RegistrationForm_password")
+    private WebElement inputPassword;
+
+    @FindBy(how= How.ID,using = "core__protected_modules_user_yiiForm_RegistrationForm_password_confirmation")
+    private WebElement inputPasswordConfirmation;
+
+    @FindBy(how= How.ID,using = "core__protected_modules_user_yiiForm_RegistrationForm_password_confirmation")
+    private WebElement comboAgree;
+
+    @FindBy(how= How.XPATH,using = "//form/fieldset[3]/button")
+    private WebElement btnRegister;
+
+    @FindBy(how= How.XPATH,using = "//form/div[2]/a")
+    private WebElement linkSignIn;
+
+    private WebDriver driver;
+
+    public  RegistrationPage(WebDriver driver){
+        this.driver=driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    public WebElement getOptionEmail() {
+        return optionEmail;
+    }
+
+    public WebElement getOptionPhoneNumber() {
+        return optionPhoneNumber;
+    }
+
+    public WebElement getInputEmail() {
+        return inputEmail;
+    }
+
+    public WebElement getInputPhoneNumber() {
+        return inputPhoneNumber;
+    }
+
+    public WebElement getOptionUSD() {
+        return optionUSD;
+    }
+
+    public WebElement getOptionEUR() {
+        return optionEUR;
+    }
+
+    public WebElement getOptionmBTC() {
+        return optionmBTC;
+    }
+
+    public WebElement getOptionmETH() {
+        return optionmETH;
+    }
+
+    public WebElement getInputPassword() {
+        return inputPassword;
+    }
+
+    public WebElement getInputPasswordConfirmation() {
+        return inputPasswordConfirmation;
+    }
+
+    public WebElement getComboAgree() {
+        return comboAgree;
+    }
+
+    public WebElement getBtnRegister() {
+        return btnRegister;
+    }
+
+    public WebElement getLinkSignIn() {
+        return linkSignIn;
+    }
+
+
 }
