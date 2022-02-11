6a34b08a654f7de6291086c2b104a99ae7673c5f
Yasmany <yasmanys88@gmail.com>
2022-02-10 23:44:32 -0300

    Applying Page Factory in HeaderLayout

--- src/test/java/pageobjects/HeaderLayout.java
+++ src/test/java/pageobjects/HeaderLayout.java
@@ -1,12 +1,36 @@
 package pageobjects;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
 public class HeaderLayout {
 
-    /*
-link button Sign IN
-   //header/div/div[2]/div[2]/a[1]
-link button Registration
-//header/div/div[2]/div[2]/a[2]
+    @FindBy(how= How.XPATH,using = "//header/div/div[2]/div[2]/a[1]")
+    private WebElement btnSigIn;
 
-    */
+    @FindBy(how= How.XPATH,using = "//header/div/div[2]/div[2]/a[2]")
+    private WebElement btnRegistration;
+
+    private WebDriver driver;
+
+    public  HeaderLayout(WebDriver driver){
+        this.driver=driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    public WebElement getBtnSigIn() {
+        return btnSigIn;
+    }
+
+    public WebElement getBtnRegistration() {
+        return btnRegistration;
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
 }
