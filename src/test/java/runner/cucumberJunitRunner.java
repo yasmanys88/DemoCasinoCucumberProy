6d948cfaa59acc4b9f85a60a38b31103d69681cd
Yasmany <yasmanys88@gmail.com>
2022-02-11 09:50:47 -0300

    Updating the structure of the defined steps

--- src/test/java/runner/cucumberJunitRunner.java
+++ src/test/java/runner/cucumberJunitRunner.java
@@ -1,4 +1,11 @@
 package runner;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"classpath:features/loginFeature.feature"},
+        glue ={"steps"}, publish = true)
 public class cucumberJunitRunner {
 }
