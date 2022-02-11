fefb0be17e724b7277bea423a32bf8c278323466
Yasmany <yasmanys88@gmail.com>
2022-02-11 00:05:58 -0300

    Applying Page Factory in Sign In page

--- src/test/java/pageobjects/SignInPage.java
+++ src/test/java/pageobjects/SignInPage.java
@@ -1,10 +1,45 @@
 package pageobjects;
 
-public class SignInPage {
-    //inputext id="UserLogin_username"
-    //inputext id="UserLogin_password"
-    //checkbox id="UserLogin_rememberMe"
-    //link with text "Forgot password?"
-    //button button--s1 button--t1     "Sign in"
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 
+public class SignInPage {
+
+    @FindBy(how= How.ID,using = "UserLogin_username")
+    private WebElement inputLoginName;
+
+    @FindBy(how= How.ID,using = "UserLogin_password")
+    private WebElement inputPassword;
+
+    @FindBy(how= How.XPATH,using = "//form/fieldset[1]/div[3]/div/label")
+    private WebElement checkRememberMe;
+
+    @FindBy(how= How.XPATH,using = "//form/fieldset[1]/div[3]/a")
+    private WebElement linkForgotPassword;
+
+    private WebDriver driver;
+
+    public  SignInPage(WebDriver driver){
+        this.driver=driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    public WebElement getInputLoginName() {
+        return inputLoginName;
+    }
+
+    public WebElement getInputPassword() {
+        return inputPassword;
+    }
+
+    public WebElement getCheckRememberMe() {
+        return checkRememberMe;
+    }
+
+    public WebElement getLinkForgotPassword() {
+        return linkForgotPassword;
+    }
 }
